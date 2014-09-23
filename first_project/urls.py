855c98a34e81e3fa61b13590b37ed7fb8765a4b6
amulius <joshua@Joshua-Stortzs-MacBook-Pro.local>
2014-09-22 20:48:21 -0700

    small change to settings

--- first_project/urls.py
+++ first_project/urls.py
@@ -37,6 +37,6 @@ urlpatterns = patterns('',
 )
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
