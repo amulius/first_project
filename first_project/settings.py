70eefbc55c6dd9ca29f4802eb1da9bb2893cec6f
amulius <joshua@Joshua-Stortzs-MacBook-Pro.local>
2014-09-22 20:41:32 -0700

    changing data

--- first_project/settings.py
+++ first_project/settings.py
@@ -84,6 +84,10 @@ USE_TZ = True
 
 # STATIC_URL = '/static/'
 
+FIXTURE_DIRS = (
+    'college_life/fixtures/',
+)
+
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, "static", *MEDIA_URL.strip("/").split("/"))
